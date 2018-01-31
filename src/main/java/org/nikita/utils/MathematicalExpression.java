7e49dc2b27fb152481ff071fc94200b613d9237c
nikita kogta <nikita@nikitas-MacBook-Pro-2.local>
2018-01-30 21:40:19 -0800

    updating MathematicalExpression

--- src/main/java/org/nikita/utils/MathematicalExpression.java
+++ src/main/java/org/nikita/utils/MathematicalExpression.java
@@ -11,40 +11,33 @@ public class MathematicalExpression {
         int result = 0;
         Stack num = new Stack();
         Stack <Character> operator = new Stack();
-        Stack tempNum = new Stack();
-        Stack <Character> tempOper = new Stack();
+        int leftside = 0;
+        int rightside = 0;
+        char curOper = ' ';
 
         for(int i=0; i < expression.length; i++){
             if(expression[i] == '+' || expression[i] == '-' || expression[i] == '*' || expression[i] == '/') {
-                operator.push(expression[i]);
+                if (!operator.isEmpty() && (isGreater(operator.peek(), expression[i]))) {
+                    rightside = Integer.parseInt(num.pop().toString());
+                    leftside = Integer.parseInt(num.pop().toString());
+                    curOper = operator.pop();
+                    result = calculate(leftside, curOper, rightside);
+                    num.push(result);
+                    operator.push(expression[i]);
+                } else {
+                    operator.push(expression[i]);
+                }
             }else{
                 num.push(expression[i]);
             }
         }
 
-        while(!num.isEmpty() || !operator.isEmpty()){
-            tempNum.push(num.pop());
-            tempOper.push(operator.pop());
-            if(!operator.isEmpty()  && !tempOper.isEmpty()){
-            while(isGreater(operator.peek(), tempOper.peek())) {
-                tempNum.push(num.pop());
-                tempOper.push(operator.pop());
-                if(num.isEmpty() || operator.isEmpty())
-                    break;
-            }}
-            result = calculate((Integer.parseInt(num.pop().toString())), tempOper.pop(), (Integer.parseInt(tempNum.pop().toString())));
+        while(!operator.isEmpty()){
+            rightside = Integer.parseInt(num.pop().toString());
+            leftside = Integer.parseInt(num.pop().toString());
+            curOper = operator.pop();
+            result = calculate(leftside, curOper, rightside);
             num.push(result);
-
-            while(num.isEmpty() || operator.isEmpty()){
-              if(!tempNum.isEmpty() && !tempOper.isEmpty()) {
-                  result = calculate(Integer.parseInt(String.valueOf(num.pop())), tempOper.pop(), Integer.parseInt(String.valueOf(tempNum.pop())));
-                  num.push(result);
-              }else{
-                  result = Integer.parseInt(num.pop().toString());
-                  break;
-              }
-
-            }
         }
         return result;
     }
@@ -78,11 +71,8 @@ public class MathematicalExpression {
     public static void main(String [] args){
         MathematicalExpression mathematicalExpression = new MathematicalExpression();
         String expr = "2+3*4-5";
-//        System.out.println(expr + " = " + mathematicalExpression.evaluate(expr.toCharArray()));
-//        expr = "2*3+4-5";
-//        System.out.println(expr + " = " + mathematicalExpression.evaluate(expr.toCharArray()));
-//        expr = "2/2+4*5";
-//        System.out.println(expr + " = " + mathematicalExpression.evaluate(expr.toCharArray()));
+        expr = "2*3+4-5";
+        expr = "2/2+4*5";
         expr = "2/2+4*5-10";
         System.out.println(expr + " = " + mathematicalExpression.evaluate(expr.toCharArray()));
     }
